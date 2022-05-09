9a2afe830c9809753d9a810b0c483ce4cdae0e63
melbellz16 <Vamp!Adklgh1>
2022-05-09 03:51:24 +0300

    ветка с удалением отметки о принадлежности к таблице TaskGroup

--- src/dbo/TaskGroup.java
+++ src/dbo/TaskGroup.java
@@ -1,7 +1,7 @@
 package dbo;
 
 public class TaskGroup extends DboAbstact {
-    private String tableName = "TaskGroup"; //помечаем принадлежность к таблице TaskGroup
+    //private String tableName = "TaskGroup"; //помечаем принадлежность к таблице TaskGroup
 //определяем конструктор
     public TaskGroup(){
         //передаем наше имя таблицы для того чтобы родительский класс вызвал конструктор в наследниках(чтобы был параметр)
